e89c983e0fd6fc0e1eb9debe58bc33668abd82f1
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-05-28 23:43:45 +0300

    test massage

--- app/Mail/SendEmail.php
+++ app/Mail/SendEmail.php
@@ -31,9 +31,44 @@ class SendEmail extends Mailable
      */
     public function build()
     {
-        return $this->from("PorstLogin@yandex.ru")
+        $to = 'Gorponffffff@yandex.ru';
+        $subject = 'Тестовое письмо с HTML';
+
+        $message = '
+        <html>
+            <head>
+                <title>Тестовое письмо с HTML</title>
+                <meta charset="utf8">
+            </head>
+            <body>
+                <p>Пример таблицы</p>
+                <table>
+                    <tr>
+                        <th>Колонка 1</th><th>Колонка 2</th><th>Колонка 3</th><th>Колонка 4</th>
+                    </tr>
+                    <tr>
+                        <td>Ячейка 1</td><td>Ячейка 2</td><td>Ячейка 3</td><td>Ячейка 4</td>
+                    </tr>
+                    <tr>
+                        <td>Ячейка 5</td><td>Ячейка 6</td><td>Ячейка 7</td><td>Ячейка 8</td>
+                    </tr>
+                </table>
+            </body>
+        </html>
+        ';
+
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=utf8';
+        $headers[] = 'To: Receiver <receiver@test.com>';
+        $headers[] = 'From: Sender <sender@test.com>';
+        $headers[] = 'Cc: copy@test.com';
+
+        $result = mail($to, $subject, $message, implode("\r\n", $headers));
+        echo $result ? 'OK' : 'Error';
+
+        /*return $this->from("PorstLogin@yandex.ru")
                     ->view("emails/send-email")
                     ->with($this->fields)
-                    ->subject("Новое письмо");
+                    ->subject("Новое письмо");*/
     }
 }
